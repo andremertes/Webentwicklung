6782b746ffb8028f9b8a78a319b400f1887ecf46
andre <andremertes@gmail.com>
2020-11-20 17:48:18 +0100

    Hallo Welt

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <title>Andre Mertes</title>
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.5.0/dist/css/bootstrap.min.css" type="text/css" />
+        <script src="https://unpkg.com/jquery@3.5.1/dist/jquery.min.js"></script>
+        <script src="https://unpkg.com/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.css">
+        <script src="https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.js"></script>
+    </head>
+    <body>
+        Hallo Welt!
+    </body>
+</html>

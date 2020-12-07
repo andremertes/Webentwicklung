da6e54cf2eaa7b1d733191701e7f24dda864c2f9
andre <andremertes@gmail.com>
2020-12-07 22:29:04 +0100

    Blatt 3 - Aufgabe 2 fertig

--- function/todoliste.php
+++ function/todoliste.php
@@ -46,21 +46,4 @@ for ( $j ; $j < $i ; $j++ ){
     echo '</tbody></table></div></div>';
 };
 
-/*
-foreach ($todos as $tabelle) {
-    $elementsafter = array_slice($tabelle, $offset, null, true);
-    echo '<div class="col-lg">
-            <div class="card">
-                <table class="table">
-                    <thead class="thead-light">
-                        <tr>
-                            <th scope="col">'.($tabelle['header']).'</th>
-                        </tr>
-                    </thead>
-                <tbody>';
-                    echo '<tr><td>'.($tabelle['aufgaben']).'</td></tr>';
-                echo '</tbody></table></div></div>';
-}
-*/
-
 ?>
